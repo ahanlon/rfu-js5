3b63d353c4649300245eb59709582b0f0a9e7ce2
ahanlon <hanlonad27@gmail.com>
2015-06-26 17:23:37 -0600

    added some html inputs

--- main.js
+++ main.js
@@ -2,15 +2,13 @@ $(document).on('ready', function() {
   
 	$('.text').on('click', function(){
 		$(this).hide().replaceWith('<input></input>');
-		console.log(addData);
 	});
 
-	var addData = $('.input');
-	// console.log(addData);
+	// $('body').on('blur', 'input', function(){
+	// 	$(input).hide();
+	// })
 
-	$(addData).on('blur', function(){
-		$(this).hide().show('.text').append(addData);
-	});
+	
 
 
 
