3cc832a9e467e206918965e9e0685ebcca052836
ahanlon <hanlonad27@gmail.com>
2015-06-29 18:06:27 -0600

    finished Part 1 of edit in place

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
 $(document).on('ready', function() {
   
 	$('.text').on('click', function(){
-		$(this).hide().replaceWith('<input></input>');
-	});
+		$(this).hide().replaceWith('<input type="text" placeholder="Enter your info here:"></input>');
+		$('input').focus();
+	})
 
-	// $('body').on('blur', 'input', function(){
-	// 	$(input).hide();
-	// })
+	$('.do-stuff').on('click', function(){
+		$(this).hide().replaceWith('<textArea type="text" placeholder="Enter your info here:" style="width", "auto">');
+		$('textArea').focus();
+	})
+
+	$('body').on('blur', 'input', function(){
+		var newTxt = $('input').val();
+		$(this).hide().replaceWith(newTxt);
+	})
+
+	$('body').on('blur', 'textArea', function(){
+		var newTxt = $('textArea').val();
+		$(this).hide().replaceWith(newTxt);
+	})
 
 	
 
 
 
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
